b29c89fcab3f52c40bb4380767b69a462abe9a70
John Hurliman <jhurliman@jhurliman.org>
2020-12-07 16:05:30 -0800

    Fix paths

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict"
 
-const EssentiaWASM = require("../../dist/essentia-wasm.module").EssentiaWASM;
-const Essentia = require("../../dist/essentia.js-core.es");
+const EssentiaWASM = require("./dist/essentia-wasm.module");
+const Essentia = require("./dist/essentia.js-core.es");
 
 // export as default for entry point to node package with the js wrapper
 const EssentiaNode = new Essentia(EssentiaWASM);
